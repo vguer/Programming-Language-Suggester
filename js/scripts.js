061455edc30aef810dd037064749ce3eafc68e98
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-05 14:50:04 -0700

    Add additional if statements in attempt at debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  $("form#quiz").submit(function() {
+  $("form#quiz").submit(function(event) {
+    const favoritesubject= parseInt($("#favorite-subject").val());
     const musicgenre= $("input:radio[name=musicgenre]:checked").val();
     const name= ("input#name").val();
     const gender= $("select#gender").val();
     const age= parseInt($("input#age").val());
-    const favoritesubject= parseInt($("#favoritesubject").val());
-    const toothbrushColor= $("#color").val();
- 
+    const color= $("#color").val();
+
+
     if (favoritesubject === 1) {
       $('#Python').show();
     } else if (favoritesubject === 2) {
@@ -16,6 +17,10 @@ $(document).ready(function() {
     } else if (favoritesubject === 4) {
       $('#Ruby').show();
     } else if (favoritesubject === 5) {
+      $('#JavaScript').show();
+    } else if (favoritesubject === 6) {
+      $('#Ruby').show();
+    } else if (favoritesubject === 7) {
       $('#JavaScript').show();
     } else {
       $('#Python').show();
