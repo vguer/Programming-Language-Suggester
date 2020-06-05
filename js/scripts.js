b53cd765a48f19652e1e26855fa3ee3cefe383ea
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-05 10:35:07 -0700

    Fix spacing in HTML, add input type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-const favoritesubject = parseInt$("#favotiresubject").val();
-
+const favoriteSubject = parseInt$("#favotiresubject").val();
+const musicGenre = $("input:radio[name=musicgenre]:checked").val();
 const favoriteColor = $("#color").val();
