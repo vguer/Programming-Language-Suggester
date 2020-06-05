5163c423c982c33839b63967f7206bb5e1060262
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-05 11:13:31 -0700

    Add scripts functions, styles, and additional input type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-const favoriteSubject = parseInt$("#favotiresubject").val();
-const musicGenre = $("input:radio[name=musicgenre]:checked").val();
-const favoriteColor = $("#color").val();
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    const name = ('$input#name').val ();
+    const gender = $("select#gender").val();
+    const age = parseInt($("input#age").val());
+    const favoriteSubject = parseInt$("#favotiresubject").val();
+    const musicGenre = $("input:radio[name=musicgenre]:checked").val();
+    const toothbrushColor = $("#color").val();
+
+    const result = 
+  });
