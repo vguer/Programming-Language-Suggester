5d11b51511f93f2441d0c75bfc49ec8befacb694
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-05 11:58:49 -0700

    Debug scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    const name = ('$input#name').val ();
-    const gender = $("select#gender").val();
-    const age = parseInt($("input#age").val());
-    const favoriteSubject = parseInt$("#favotiresubject").val();
-    const musicGenre = $("input:radio[name=musicgenre]:checked").val();
-    const toothbrushColor = $("#color").val();
+    let name = ("input#name").val ();
+    let gender = $("select#gender").val();
+    let age = parseInt($("input#age").val());
+    let favoriteSubject = parseInt($("#favotiresubject").val());
+    let musicGenre = $("input:radio[name=musicgenre]:checked").val();
+    let toothbrushColor = $("#color").val();
 
-    const result = musicGenre
     if (musicGenre === 'Unblackmetal') {
       $('#Unblackmetal').show();
     } else if (musicGenre === 'Medievalfolkrock') {
@@ -18,9 +17,10 @@ $(document).ready(function() {
       $('#Germanreggae').show();
     } else if (musicGenre ==='Mathrock') {
       $('#Germanreggae').show();
-    } else (musicGenre === 'Regrass') {
+    } else (musicGenre === 'Regrass') 
       $('#Redgrass').show();
-    }
-    event.preventDefault()
+      
+      event.preventDefault()
+    })
+  
   });
-});
