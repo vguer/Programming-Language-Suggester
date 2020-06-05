00161f8d57da1a6c01337f31058e6b2e07c0c5bd
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-05 14:03:42 -0700

    Modify braching to different input

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
   $("form#quiz").submit(function() {
-    event.preventDefault()
     const musicgenre= $("input:radio[name=musicgenre]:checked").val();
-    const name= ("input#name").val ();
+    const name= ("input#name").val();
     const gender= $("select#gender").val();
     const age= parseInt($("input#age").val());
-    const favoriteSubject= parseInt($("#favotiresubject").val());
+    const favoritesubject= parseInt($("#favoritesubject").val());
     const toothbrushColor= $("#color").val();
-
-    if (musicGenre === 'Unblackmetal') {
+ 
+    if (favoritesubject === 1) {
       $('#Python').show();
-    } else if (musicGenre === 'Medievalfolkrock') {
+    } else if (favoritesubject === 2) {
       $('#Ruby').show();
-    } else if (musicGenre === 'Shoegaze') {
+    } else if (favoritesubject === 3) {
       $('#JavaScript').show(); 
-    } else if (musicGenre === 'Germanreggae') {
+    } else if (favoritesubject === 4) {
       $('#Ruby').show();
-    } else if (musicGenre ==='Mathrock') {
+    } else if (favoritesubject === 5) {
       $('#JavaScript').show();
-    } else (musicGenre === 'Regrass') 
+    } else {
       $('#Python').show();
-
-      event.preventDefault()
-    })
-  
+    }
+    event.preventDefault()
   });
+});
