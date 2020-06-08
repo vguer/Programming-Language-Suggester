829f6f647e4a26cab6acb11f5720ffd53ded8d3b
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-07 23:42:37 -0700

    Fix bug that was not showing results previously

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    let favoritesubject = parseInt($("#favorite-subject").val());
+    const favoritesubject = parseInt($("#favorite-subject").val());
     const musicgenre = $("input:radio[name=musicgenre]:checked").val();
     const name = ($("input#name").val());
     const gender = $("select#gender").val();
     const age = parseInt($("input#age").val());
     const color = $("#color").val();
 
+    const result = favoritesubject;
     if (favoritesubject === 1) {
-      $('#Python').show();
+      $("#Python").show();
     } else if (favoritesubject === 2) {
-      $('#Ruby').show();
+      $("#Ruby").show();
     } else if (favoritesubject === 3) {
-      $('#JavaScript').show(); 
+      $("#JavaScript").show(); 
     } else if (favoritesubject === 4) {
-      $('#Ruby').show();
+      $("#Ruby").show();
     } else if (favoritesubject === 5) {
-      $('#JavaScript').show();
+      $("#JavaScript").show();
     } else if (favoritesubject === 6) {
-      $('#Ruby').show();
+      $("#Ruby").show();
     } else if (favoritesubject === 7) {
-      $('#JavaScript').show();
+      $("#JavaScript").show();
     } else {
-      $('#Python').show();
+      $("#Python").show();
     }
+
     event.preventDefault()
   });
 });
